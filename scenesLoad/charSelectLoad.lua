23a4a65bc0d5f0eddde881639b834f06a9855faf
Xudong Liu <112330682+KuroeTaro@users.noreply.github.com>
2024-06-10 19:29:04 -0400

    Update charSelectLoad.lua

--- scenesLoad/charSelectLoad.lua
+++ scenesLoad/charSelectLoad.lua
@@ -568,12 +568,10 @@ function loadShaderOfCharSelectScene()
     --These two shaders were found on shadertoy, I just embedded these two shaders into love2d.
     fractalNoiseShader = love.graphics.newShader[[
         extern float time;
-        
         float hash(float x)
         {
             return mod(sin(cos(x * 12.13) * 19.123) * 17.321, 1.0);
         }
-
         float noise(vec2 p)
         {
             vec2 pm = mod(p, 1.0);
@@ -586,15 +584,12 @@ function loadShaderOfCharSelectScene()
             v2 = mix(v2, v3, smoothstep(0.0, 1.0, pm.x));
             return mix(v0, v2, smoothstep(0.0, 1.0, pm.y));
         }
-
         vec4 effect(vec4 color, Image texture, vec2 texture_coords, vec2 screen_coords)
         {
             vec2 uv = screen_coords.xy / 900;
-
             uv *= 0.8;
-
             float v = 0.0;
-
+    
             for (float i = 0.0; i < 12.0; i += 1.0)
             {
                 float t = mod(1 + i, 12.0);
@@ -602,17 +597,15 @@ function loadShaderOfCharSelectScene()
                 float e = exp2(t);
                 v += noise(uv * e + vec2(time*1, 1)) * (1.0 - (t / 12.0)) * (t / 12.0);
             }
-
+    
             v -= 1;
             v = v * 2;
             v = v + 0.2;
-
             if(v< 0){
                 v = 0;
             }else if(v > 1){
                 v = 1;
             }
-
             return vec4(v, v, v, 1.0);
         }
     ]]
@@ -639,7 +632,6 @@ function loadShaderOfCharSelectScene()
             }
 
             finalColor /= float(nsamples);
-
             return finalColor;
         }
     ]]
